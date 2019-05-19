771e29580183ee5322cd606147a8104f4695563c
Diana De Leon <dianaximeo@gmail.com>
2019-05-18 19:23:15 -0600

    Add UI

--- Assets/Scripts/SphereBehaviour.cs
+++ Assets/Scripts/SphereBehaviour.cs
@@ -7,14 +7,21 @@ public class SphereBehaviour : MonoBehaviour
 {
 
     public Rigidbody rb;
-
-    public float velocity = -10;
+    public Slider masa;
+    public Slider velocidad;
+    private float velocity;
+    private float mass;
     // Start is called before the first frame update
     void Start()
     {
+        velocidad = transform.Find("Canvas").Find("Vinicial").GetComponent<Slider>();
+        velocity = velocidad.value;
+        masa = transform.Find("Canvas").Find("Masa").GetComponent<Slider>();
+        mass = masa.value;
         rb = transform.GetComponent<Rigidbody>();
         if (rb)
         {
+            rb.mass = mass;
             rb.AddForce(velocity,0,0,ForceMode.Impulse);
             
         }
@@ -24,8 +31,11 @@ public class SphereBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        velocity = velocidad.value;
+        mass = masa.value;
         transform.Find("Canvas").Find("Velocidad").GetComponent<Text>().text = rb.velocity.ToString();
-        
+        transform.Find("Canvas").Find("Vinicial").Find("printvi").GetComponent<Text>().text = velocity.ToString();
+        transform.Find("Canvas").Find("Masa").Find("printmasa").GetComponent<Text>().text = mass.ToString();
     }
 
     private void OnCollisionEnter(Collision collision)
