623cf80e7df48b58295a0d2223058b811ac1743a
Gus Mendez <gus.mendez.99@gmail.com>
2019-05-21 22:35:51 -0600

    GUI added to show angular velocity/momentum

--- Assets/Scripts/SphereBehaviour.cs
+++ Assets/Scripts/SphereBehaviour.cs
@@ -25,6 +25,13 @@ public class SphereBehaviour : MonoBehaviour
     private Vector3 l_barra; //Largo de la barra (solo para calculo)
     private float large; //Largo de la barra
     private float cm_barra; //Centro de masa de la barra
+
+    private float angularVelocity;
+    private float angularMomentum;
+
+
+    private bool hasCollided; //For show angular n inertial info.
+
     void Start()
     {
         rbe = transform.GetComponent<Rigidbody>();
@@ -33,6 +40,12 @@ public class SphereBehaviour : MonoBehaviour
         l_barra = punto_inicial.transform.position - punto_final.transform.position;
         large = l_barra.z;
         cm_barra = large / 2;
+
+        hasCollided = false;
+        angularVelocity = 0.0f;
+        angularMomentum = 0.0f;
+
+
     }
 
     // Update is called once per frame
@@ -49,12 +62,14 @@ public class SphereBehaviour : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         //True = Colision elastica
+        hasCollided = true;
         if (typecol == true)
         {
             p_impacto = gameObject.transform.position;
             float vel = rbe.velocity.x;
             rbb.velocity = new Vector3(Vel_B(vel, rbe.mass, rbb.mass, large, cm_barra, p_impacto), 0, 0);
             rbb.angularVelocity = new Vector3(Vel_Ang_Elastica(vel,rbe.mass, rbb.mass,large,cm_barra,p_impacto),0,0);
+            this.angularVelocity = Vel_Ang_Elastica(vel, rbe.mass, rbb.mass, large, cm_barra, p_impacto);
             rbe.velocity = new Vector3(Vel_final_esf(vel, rbe.mass, rbb.mass, large, cm_barra, p_impacto), 0, 0);
         }
         //False = Colision Inelastica 
@@ -65,6 +80,7 @@ public class SphereBehaviour : MonoBehaviour
             p_impacto =gameObject.transform.position;
             float veli = rbe.velocity.x;
             rbb.velocity = new Vector3(Vel_CM(veli,rbe.mass, rbb.mass),0,0);
+            this.angularVelocity = Vel_Ang_Inelastica(veli, rbe.mass, rbb.mass, cm_barra, p_impacto, large);
             rbb.angularVelocity = new Vector3(Vel_Ang_Inelastica(veli,rbe.mass, rbb.mass,cm_barra,p_impacto,large), 0, 0);
         }
     }
@@ -96,6 +112,7 @@ public class SphereBehaviour : MonoBehaviour
     float Vel_Ang_Elastica(float ve_in, float masa1, float masa2, float largo, float cmactual, Vector3 dis)
     {
         float vel_angular = 0;
+        this.angularMomentum = Icm_barra(largo, masa2);
         vel_angular = (-1) * ((Brazo(cmactual, dis) * masa2 * Vel_B(ve_in, masa1, masa2, largo, cmactual, dis))/Icm_barra(largo, masa2));
         return vel_angular;
     }
@@ -104,6 +121,7 @@ public class SphereBehaviour : MonoBehaviour
     float Vel_Ang_Inelastica(float ve_in, float masa1, float masa2, float cm_actual, Vector3 dis, float largo)
     {
         float vel_angular = 0;
+        this.angularMomentum = Icm_total(cm_actual, dis, largo, masa1, masa2);
         vel_angular = (Brazo(cm_actual, dis) * ve_in * masa1) / Icm_total(cm_actual, dis, largo, masa1, masa2);
         return vel_angular;
     }
@@ -161,5 +179,18 @@ public class SphereBehaviour : MonoBehaviour
         Vfinal = ve_in - ((masa2 / masa1) * Vel_B(ve_in, masa1, masa2, largo, cmactual, dis));
         return Vfinal;
     }
+
+    void OnGUI()
+    {
+        if (hasCollided)
+        {
+            string message = "La velocidad angular es de " + -this.angularVelocity + " rad/s";
+            message += "\nEl momento de inercia de la barra es " + this.angularMomentum + " kg·m²/s.";
+
+            GUI.Box(new Rect(Screen.width / 2 - 150, Screen.height - 70, 300, 150), message);
+        }
+
+    }
+
 }
 
